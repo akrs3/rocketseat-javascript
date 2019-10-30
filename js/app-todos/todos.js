2bccef5a18ba230cb1709952885322937018927b
Anne Kelly <ctinfoapple2@iMac-de-Anne.local>
2019-10-30 11:24:40 -0300

    Excluindo Todos

--- js/app-todos/todos.js
+++ js/app-todos/todos.js
@@ -16,7 +16,19 @@ function renderTodos() {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir');
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
         listElement.appendChild(todoElement);
     }
 }
@@ -33,3 +45,8 @@ function addTodo(){
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+}
